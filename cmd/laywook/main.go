4e898e32f06fe9f397ff5b46a604b63fab6748e4
Syahrul Ardiansyah <syahrul.findi@gmail.com>
2020-11-21 06:47:39 +0700

    Init server + go mod

--- cmd/laywook/main.go
+++ cmd/laywook/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	fmt.Println("Hello World!")
+
+	r := http.NewServeMux()
+
+	fmt.Printf("Starting beanie at http://localhost:%s/\n", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
 }
